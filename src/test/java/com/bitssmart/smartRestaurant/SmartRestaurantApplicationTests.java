298a3e8af37a34f7e690eb84f052a07d248d2396
Avinash <vintas@users.noreply.github.com>
2020-12-15 22:13:10 +0530

    spring boot scaffolding

---
+++ src/test/java/com/bitssmart/smartRestaurant/SmartRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitssmart.smartRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
