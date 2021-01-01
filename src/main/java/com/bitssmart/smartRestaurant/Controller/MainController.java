2e283a950722d5d0ff979b3272f2dd714be18e18
Prachi <75129191+PrachiSGodbole@users.noreply.github.com>
2021-01-01 18:15:50 +0530

    prototype, view template added

---
+++ src/main/java/com/bitssmart/smartRestaurant/Controller/MainController.java
@@ -0,0 +1,22 @@
+package com.bitssmart.smartRestaurant.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/home")    
+	public String home()  
+	{
+		return "index";
+	}
+	@RequestMapping("/menu")    
+	public String menu()  
+	{
+		return "menu";
+	}
+	/*
+	 * @RequestMapping("/") public String home() { return "greeting"; }
+	 */
+}
