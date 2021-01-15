8fda5c305704e5982856c8d58dc64f388e0f620b
souveekp <souveek.rishi123@gmail.com>
2021-01-16 00:36:51 +0530

    Temporary Souveek progress1

--- src/main/java/com/bitssmart/smartRestaurant/Model/Customer.java
+++ src/main/java/com/bitssmart/smartRestaurant/Model/Customer.java
@@ -67,4 +67,28 @@ public class Customer {
 	@JsonIgnore
 	@PrimaryKeyJoinColumn
 	private List<FoodOrder> orderId;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
