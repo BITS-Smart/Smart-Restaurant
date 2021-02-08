c65fc5c0effadfd9afb0d1c3c9f505615f5d8175
Prachi <75129191+PrachiSGodbole@users.noreply.github.com>
2021-02-08 20:43:17 +0530

    manager landing page

--- src/main/java/com/bitssmart/smartRestaurant/Repository/UserRepository.java
+++ src/main/java/com/bitssmart/smartRestaurant/Repository/UserRepository.java
@@ -1,12 +1,20 @@
 package com.bitssmart.smartRestaurant.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import com.bitssmart.smartRestaurant.Model.FoodOrder;
 import com.bitssmart.smartRestaurant.Model.User;
 
 @Repository("userRepository")
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	 User findByEmail(String loginId);
+	 
+	 @Query("SELECT a FROM User a WHERE deliveryGuy.isApproved= :boolVal")
+	 public List<User> findByDeliveryGuy(@Param("boolVal") Boolean val);
 }
--- src/main/java/com/bitssmart/smartRestaurant/Service/UserService.java
+++ src/main/java/com/bitssmart/smartRestaurant/Service/UserService.java
@@ -1,9 +1,13 @@
 package com.bitssmart.smartRestaurant.Service;
 
+import java.util.List;
+
 import com.bitssmart.smartRestaurant.Model.User;
 
 public interface UserService {
 	 public User findUserByEmail(String email);
 	 
 	 public void saveUser(User user);
+	 
+	 public List<User> findByDeliveryGuy();
 }
